9780b65c782a9cd07d0f27e6b45bb7702c9434be
Aakash Soni <aakash.soni@findingpi.com>
2025-07-26 14:32:40 +0530

    Refactor database connection configuration to use individual environment variables for user, host, database, password, and port, enhancing security and flexibility.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,20 @@
 import { Pool } from "pg";
-console.log("DBurl", process.env.DATABASE_URL);
+
+console.log("DB_USER", process.env.DB_USER);
+console.log("DB_HOST", process.env.DB_HOST);
+console.log("DB_NAME", process.env.DB_NAME);
+console.log("DB_PASSWORD", process.env.DB_PASSWORD);
+console.log("DB_PORT", process.env.DB_PORT);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT as string),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
